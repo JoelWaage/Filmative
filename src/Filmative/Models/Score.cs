97f3ead6bbd171aecd59e6accbc25c8578ac9eb1
Joel <joeltwaage@gmail.com>
2016-09-27 16:38:21 -0700

    Add basic Score Model Class

---
+++ src/Filmative/Models/Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Filmative.Models
+{
+    [Table("Scores")]
+    public class Score
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
