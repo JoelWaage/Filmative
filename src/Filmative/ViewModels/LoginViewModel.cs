4e1159789b8d31d67ae6f719d275ce4632b9cd3a
epicodus.student <nothing@>
2016-09-30 18:09:42 -0700

    Create LoginViewModel.cs

---
+++ src/Filmative/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filmative.ViewModels;
+
+namespace Filmative.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
