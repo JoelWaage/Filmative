8b743c823e50dd738cbadd0ad792a7dfe59a829b
Joel <joeltwaage@gmail.com>
2016-10-04 10:08:24 -0700

    Add and pass MovieControllerTest.cs

---
+++ Filmative.Tests/ControllerTests/MoviesControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Filmative.Controllers;
+using Filmative.Models;
+using Xunit;
+
+namespace Filmative.Tests.ControllerTests
+{
+    public class MoviesControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            MoviesController controller = new MoviesController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
