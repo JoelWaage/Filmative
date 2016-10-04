5007dd12ff7efcedaf86cd26d643ed877aaae41a
Joel <joeltwaage@gmail.com>
2016-10-04 09:53:28 -0700

    Add xunit test for getting movie title

---
+++ Filmative.Tests/MovieTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filmative.Models;
+using Xunit;
+
+namespace Filmative.Tests
+{
+    public class MovieTest
+    {
+       [Fact]
+       public void GetTitleTest()
+        {
+            var movie = new Movie();
+            movie.Title = "Tron";
+
+            var result = movie.Title;
+
+            Assert.Equal("Tron", result);
+        }
+    }
+}
